e93e249c2ab11b81bed2470de91d689ac17871db
Mahmudul Alam <expelmahmud@gmail.com>
2022-04-01 15:57:28 +0600

    CommonJS exports

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,38 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/button-styles.js":
+/*!******************************!*\
+  !*** ./src/button-styles.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("const red = 'color: red;';\nconst blue = 'color:  blue;';\nconst makeColorStyle = (color) => `color: ${color};`;\n\nexports.red = red;\nexports.blue = blue;\nexports.makeColorStyle = makeColorStyle;\n\n\n//# sourceURL=webpack://debug-script/./src/button-styles.js?");
+
+/***/ }),
+
+/***/ "./src/button.js":
+/*!***********************!*\
+  !*** ./src/button.js ***!
+  \***********************/
+/***/ ((module) => {
+
+eval("// take a str, the button label and return an element\n/**\n *\n * @param {string} buttonName\n * @returns {Element}\n */\nconst makeButton = (buttonName) => {\n  return `Button: ${buttonName}`;\n};\n\nmodule.exports = makeButton;\n\n\n//# sourceURL=webpack://debug-script/./src/button.js?");
+
+/***/ }),
+
+/***/ "./src/footer.js":
+/*!***********************!*\
+  !*** ./src/footer.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"bottom\": () => (/* binding */ bottom),\n/* harmony export */   \"top\": () => (/* binding */ top)\n/* harmony export */ });\nconst top = 'top';\nconst bottom = 'bottom';\n\n\n//# sourceURL=webpack://debug-script/./src/footer.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
@@ -16,7 +46,8 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav */ \"./src/nav.js\");\n\n\nconsole.log(_nav__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\n\n\n//# sourceURL=webpack://debug-script/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav */ \"./src/nav.js\");\n/* harmony import */ var _footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./footer */ \"./src/footer.js\");\n/* harmony import */ var _button__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./button */ \"./src/button.js\");\n/* harmony import */ var _button__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_button__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _button_styles__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./button-styles */ \"./src/button-styles.js\");\n\n\n\n\n\nconsole.log((0,_nav__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(), _footer__WEBPACK_IMPORTED_MODULE_1__.top, _footer__WEBPACK_IMPORTED_MODULE_1__.bottom, _button__WEBPACK_IMPORTED_MODULE_2___default()('My first button!'), (0,_button_styles__WEBPACK_IMPORTED_MODULE_3__.makeColorStyle)('cyan'));\n\n\n//# sourceURL=webpack://debug-script/./src/index.js?");
 
 /***/ }),
 
@@ -26,6 +57,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nav
   \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (() => 'nav');\n\n\n//# sourceURL=webpack://debug-script/./src/nav.js?");
 
 /***/ })
@@ -57,6 +89,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
---
+++ src/button.js
@@ -0,0 +1,11 @@
+// take a str, the button label and return an element
+/**
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+const makeButton = (buttonName) => {
+  return `Button: ${buttonName}`;
+};
+
+module.exports = makeButton;
