f0d80558ad730c772d8d052e05877e3ace6b287d
Mahmudul Alam <expelmahmud@gmail.com>
2022-04-02 15:20:27 +0600

    Webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
+const { merge } = require('webpack-merge');
 
-module.exports = (env, { mode }) => {
-  console.log(mode); // this way you can see what mode is
-  return {
-    mode,
-    output: {
-      filename: 'bundle.js',
+module.exports = (env, { mode, presets } = { mode: 'production', presets: [] }) => {
+  return merge(
+    {
+      mode,
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+    modeConfig(mode)
+    // loadPresets({ mode, presets })
+  );
 };
