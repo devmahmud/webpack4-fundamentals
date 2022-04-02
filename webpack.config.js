fb5ba78757e4d0375786cde12e2001e22f890c31
Mahmudul Alam <expelmahmud@gmail.com>
2022-04-02 14:14:28 +0600

    Webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-module.exports = (env, argv) => {
-  console.log(env, argv);
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, { mode }) => {
+  console.log(mode); // this way you can see what mode is
   return {
-    mode: argv.mode,
+    mode,
     output: {
       filename: 'bundle.js',
     },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
